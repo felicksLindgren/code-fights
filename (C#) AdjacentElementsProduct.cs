2918a6b985a1fa28de815cf56dabb9b64c4915fe
Felix Lindgren <felix.p.lindgren@gmail.com>
2017-10-26 17:19:07 +0200

    Create (C#) AdjacentElementsProduct.cs

---
+++ (C#) AdjacentElementsProduct.cs
@@ -0,0 +1,7 @@
+int adjacentElementsProduct(int[] inputArray) {
+    int max = -2500;
+    for (int i=1; i<inputArray.Length;) {
+        max = Math.Max(max, inputArray[i-1] * inputArray[i++]);
+    }
+    return max;
+}
