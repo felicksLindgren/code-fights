daec2819efa4350320baf4f1135c51bfa98762e3
Felix Lindgren <felix.p.lindgren@gmail.com>
2017-10-26 17:20:42 +0200

    Update (C#) AdjacentElementsProduct.cs

--- (C#) AdjacentElementsProduct.cs
+++ (C#) AdjacentElementsProduct.cs
@@ -1,3 +1,5 @@
+//Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
 int adjacentElementsProduct(int[] inputArray) {
     int max = -2500;
     for (int i=1; i<inputArray.Length;) {
