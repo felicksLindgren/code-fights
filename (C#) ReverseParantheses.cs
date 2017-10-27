c1c4bdd5884280f561f403831e65de20de5079f8
Felix Lindgren <felix.p.lindgren@gmail.com>
2017-10-27 10:13:15 +0200

    Update (C#) ReverseParantheses.cs

--- (C#) ReverseParantheses.cs
+++ (C#) ReverseParantheses.cs
@@ -1,8 +1,10 @@
 string reverseParentheses(string s) {
-    var l = s.LastIndexOf('(');
-    if (l == -1) return s;
-    var r = s.IndexOf(')', l);
-    var arr = s.Substring(l + 1, r - l - 1).ToCharArray();
-    Array.Reverse(arr);
-    return reverseParentheses(s.Substring(0, l) + new string(arr) + s.Substring(r + 1));
+    while(true){
+        var l = s.LastIndexOf('(');
+        if (l == -1) return s;
+        var r = s.IndexOf(')', l);
+        var arr = s.Substring(l + 1, r - l - 1).ToCharArray();
+        Array.Reverse(arr);
+        s = s.Substring(0, l) + new string(arr) + s.Substring(r + 1);
+    }
 }
